c400202a3885f933cbd136e10eb63e63d21eaf0d
matheusTA <matrajano12@gmail.com>
2025-01-23 22:42:11 -0300

    feat: add late check-in validation error and implement validation logic

--- src/use-cases/validate-check-in.spec.ts
+++ src/use-cases/validate-check-in.spec.ts
@@ -1,7 +1,8 @@
-import { describe, it, expect, beforeEach } from "vitest";
+import { describe, it, expect, beforeEach, afterEach, vi } from "vitest";
 import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository";
 import { ValidateCheckInUseCase } from "./validate-check-in";
 import { ResourceNotFoundError } from "./errors/resource-not-found";
+import { LateCheckInValidationError } from "./errors/late-check-in-validation-error";
 
 let checkInsRepository: InMemoryCheckInsRepository;
 let validateCheckInUseCase: ValidateCheckInUseCase;
@@ -11,12 +12,12 @@ describe("validate check in use case", () => {
     checkInsRepository = new InMemoryCheckInsRepository();
     validateCheckInUseCase = new ValidateCheckInUseCase(checkInsRepository);
 
-    // vi.useFakeTimers();
+    vi.useFakeTimers();
   });
 
-  // afterEach(() => {
-  //   vi.useRealTimers();
-  // });
+  afterEach(() => {
+    vi.useRealTimers();
+  });
 
   it("should be to validate able to check-in", async () => {
     const createdCheckIn = await checkInsRepository.create({
@@ -35,5 +36,21 @@ describe("validate check in use case", () => {
     await expect(() =>
       validateCheckInUseCase.execute({ checkInId: "invalid-id" }),
     ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+
+  it("should not be able to validate the check-in 20 minutes of its creation", async () => {
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40));
+
+    const createdCheckIn = await checkInsRepository.create({
+      userId: "user-01",
+      gymId: "gym-01",
+    });
+
+    const twentyOneMinutesInMilliseconds = 21 * 60 * 1000;
+    vi.advanceTimersByTime(twentyOneMinutesInMilliseconds);
+
+    await expect(() =>
+      validateCheckInUseCase.execute({ checkInId: createdCheckIn.id }),
+    ).rejects.toBeInstanceOf(LateCheckInValidationError);
   });
 });
