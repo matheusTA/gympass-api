24ef09ea6801285d1a5a01a51a3f0925da16d789
matheusTA <matrajano12@gmail.com>
2025-01-26 21:53:14 -0300

    feat: add tests for check-in creation, history, metrics, and validation controllers

---
+++ src/http/controllers/check-ins/history.spec.ts
@@ -0,0 +1,52 @@
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import request from "supertest";
+import { app } from "@/app";
+import { createAndAuthenticateUser } from "@/utils/test/create-and-authenticate-user";
+import { prisma } from "@/config/prisma";
+
+describe("check-in history controller", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to get check-in history", async () => {
+    const { token } = await createAndAuthenticateUser(app);
+
+    const user = await prisma.user.findFirstOrThrow();
+
+    const gym = await prisma.gym.create({
+      data: {
+        title: "Gym 01",
+        latitude: -8.0412672,
+        longitude: -34.9077504,
+      },
+    });
+
+    await prisma.checkIn.createMany({
+      data: [
+        {
+          userId: user.id,
+          gymId: gym.id,
+          createdAt: new Date("2021-01-01T00:00:00Z"),
+        },
+        {
+          userId: user.id,
+          gymId: gym.id,
+          createdAt: new Date("2021-01-02T00:00:00Z"),
+        },
+      ],
+    });
+
+    const response = await request(app.server)
+      .get("/check-ins/history")
+      .set("Authorization", `Bearer ${token}`)
+      .send();
+
+    expect(response.status).toBe(200);
+    expect(response.body.checkIns).toHaveLength(2);
+  });
+});
