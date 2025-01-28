10c5e0413a933a47b7519ff2068505aa4b1f28db
matheusTA <matrajano12@gmail.com>
2025-01-27 21:21:04 -0300

    feat: implement refresh token functionality with cookie support

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -20,8 +20,20 @@ export async function authenticateController(
     const { user } = await authenticateUseCase.execute({ email, password });
 
     const token = await reply.jwtSign({}, { sign: { sub: user.id } });
+    const refreshToken = await reply.jwtSign(
+      {},
+      { sign: { sub: user.id, expiresIn: "7d" } },
+    );
 
-    return reply.status(200).send({ token });
+    return reply
+      .setCookie("refreshToken", refreshToken, {
+        path: "/",
+        secure: true,
+        sameSite: true,
+        httpOnly: true,
+      })
+      .status(200)
+      .send({ token });
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: error.message });
--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vitest/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { resolve } from 'node:path'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
