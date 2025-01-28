ab4f2ac3869a890cdc6e1324c1b86f132c79126f
matheusTA <matrajano12@gmail.com>
2025-01-27 21:50:05 -0300

    feat: enhance JWT handling by including user role and implementing role-based access control middleware

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -3,11 +3,17 @@ import { FastifyInstance } from "fastify";
 import { createGymController } from "./create-gym";
 import { searchGymsController } from "./search-gyms";
 import { getNearbyGymsController } from "./get-nearby-gyms";
+import { verifyUserRole } from "@/http/middlewares/varify-user-role";
+import { Role } from "@prisma/client";
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJWT);
 
   app.get("/gyms/search", searchGymsController);
   app.get("/gyms/nearby", getNearbyGymsController);
-  app.post("/gyms", createGymController);
+  app.post(
+    "/gyms",
+    { onRequest: [verifyUserRole(Role.ADMIN)] },
+    createGymController,
+  );
 }
--- src/http/controllers/users/refresh.ts
+++ src/http/controllers/users/refresh.ts
@@ -6,9 +6,14 @@ export async function refreshController(
 ) {
   await request.jwtVerify({ onlyCookie: true });
 
-  const token = await reply.jwtSign({}, { sign: { sub: request.user.sub } });
+  const role = request.user.role;
+
+  const token = await reply.jwtSign(
+    { role },
+    { sign: { sub: request.user.sub } },
+  );
   const refreshToken = await reply.jwtSign(
-    {},
+    { role },
     { sign: { sub: request.user.sub, expiresIn: "7d" } },
   );
 
